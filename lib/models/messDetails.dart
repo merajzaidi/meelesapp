ccd59bd8edd8ceb3278a34b697da019d81dc1586
praveensicker <pravin.1721it1061@kiet.edu>
2020-10-02 09:25:42 +0530

    weeklyMenuModelupdated

--- lib/models/messDetails.dart
+++ lib/models/messDetails.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
 class MessDetails {
+  final String messDetailsId;
   final String messName;
   final String messLocation;
   final DateTimeRange messTiming;
   final int messCapacitySeats;
   final bool openForWholeDay;
-  final bool singleVariant;
+  final bool singleVariantThali;
   final bool monthlyPayVariant;
   final String messImageUrl;
 
   MessDetails(
-      {@required this.messName,
-      this.messLocation,
-      this.messTiming,
-      this.messCapacitySeats,
-      this.openForWholeDay,
-      this.singleVariant,
-      this.monthlyPayVariant,
-      this.messImageUrl});
+      {@required this.messDetailsId,
+      @required this.messName,
+      @required this.messLocation,
+      @required this.messTiming,
+      @required this.messCapacitySeats,
+      @required this.openForWholeDay,
+      @required this.singleVariantThali,
+      @required this.monthlyPayVariant,
+      @required this.messImageUrl});
 }
